58457b3cf02e9088444ab0c28b7765f039b041ee
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-06-18 12:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (75.60%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,7 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
-       int low = 0, mid = 0, high = nums.length - 1;
-    while (mid <= high) {
+       int low = 0, mid = 0, end = nums.length - 1;
+    while (mid <= end) {
         if (nums[mid] == 0) {
             int tmp = nums[low];
             nums[low++] = nums[mid];
@@ -10,8 +10,8 @@ class Solution {
             mid++;
         } else {
             int tmp = nums[mid];
-            nums[mid] = nums[high];
-            nums[high--] = tmp;
+            nums[mid] = nums[end];
+            nums[end--] = tmp;
         } 
     }
 }
