9929276b4c3bb6c3477dce4c7a08b92c25c33998
Jitta Nagasai Reddy <jittanagasai16@gmail.com>
2026-08-22 22:46:21 +0530

    Time: 7 ms (2.49%) | Memory: 42.7 MB (9.96%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int s=n;
+        int sum=0;
+        int product = 1;
+        while(n!=0)
+        {
+            sum+=n%10;
+            product*=(n%10);
+            n=n/10;
+        }
+    System.out.println(sum+" "+product);
+    int plus =product+sum;
+     if(s%plus==0) return true;
+   else return false;
+    }
+}
